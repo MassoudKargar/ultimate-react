3abb36145fb1c85f7c99d550ec0f97275cacd742
MassoudKargar <75643194+MassoudKargar@users.noreply.github.com>
2025-08-24 20:44:31 +0330

    test commit

--- 05/src/App.js
+++ 05/src/App.js
@@ -1,32 +1,32 @@
-import React, { useEffect, useState, useCallback } from "react";
+import React, {useCallback, useEffect, useState} from "react";
 
 function App() {
-  const [advice, setAdvice] = useState("");
-  const [count, setCount] = useState(0);
+    const [advice, setAdvice] = useState("");
+    const [count, setCount] = useState(0);
 
 
-  const getAdvice = useCallback(async () => {
-    const response = await fetch("https://api.adviceslip.com/advice");
-    const data = await response.json();
-    setAdvice(data.slip.advice);
-    setCount(count => count + 1);
-  }, []);
+    const getAdvice = useCallback(async () => {
+        const response = await fetch("https://api.adviceslip.com/advice");
+        const data = await response.json();
+        setAdvice(data.slip.advice);
+        setCount(count => count + 1);
+    }, []);
 
-  useEffect(() => {
-    getAdvice();
-  }, [getAdvice]);
+    useEffect(() => {
+        getAdvice();
+    }, [getAdvice]);
 
-  return (
-    <div>
-      <h1>{advice}</h1>
-      <button onClick={getAdvice}>Get Advice</button>
-      <Message count={count} />
-    </div>
-  );
+    return (
+        <div>
+            <h1>{advice}</h1>
+            <button onClick={getAdvice}>Get Advice</button>
+            <Message count={count}/>
+        </div>
+    );
 }
 
 const Message = (props) => {
-  return <p>Advice fetched: {props.count} times</p>
+    return <p>Advice fetched: {props.count} times</p>
 }
 
 export default App;
