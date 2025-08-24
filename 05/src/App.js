b5922eeccfa80c21d0e66213a9c45ff4250105a4
MassoudKargar <75643194+MassoudKargar@users.noreply.github.com>
2025-08-24 15:54:22 +0330

    create first app

---
+++ 05/src/App.js
@@ -0,0 +1,32 @@
+import React, { useEffect, useState, useCallback } from "react";
+
+function App() {
+  const [advice, setAdvice] = useState("");
+  const [count, setCount] = useState(0);
+
+
+  const getAdvice = useCallback(async () => {
+    const response = await fetch("https://api.adviceslip.com/advice");
+    const data = await response.json();
+    setAdvice(data.slip.advice);
+    setCount(count => count + 1);
+  }, []);
+
+  useEffect(() => {
+    getAdvice();
+  }, [getAdvice]);
+
+  return (
+    <div>
+      <h1>{advice}</h1>
+      <button onClick={getAdvice}>Get Advice</button>
+      <Message count={count} />
+    </div>
+  );
+}
+
+const Message = (props) => {
+  return <p>Advice fetched: {props.count} times</p>
+}
+
+export default App;
