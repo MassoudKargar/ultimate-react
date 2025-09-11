eea26fe21f3bdc146e23e70e92d78424c16b8416
MassoudKargar <75643194+MassoudKargar@users.noreply.github.com>
2025-09-11 13:46:48 +0330

    finish video 32

--- 03-pizza-menu/src/App.jsx
+++ 03-pizza-menu/src/App.jsx
@@ -1,16 +1,7 @@
-import {useEffect, useState} from 'react'
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState("Hello React!")
-  useEffect(() => {
-    setCount("Hello React!");
-  }, [setCount])
-  return (
-    <>
-      <h1>{count}</h1>
-    </>
-  )
+function Pizza(props) {
+  return <h2>Pizza</h2>
 }
-
-export default App
+ 
+export default function App() {
+  return <Pizza/>;
+}
