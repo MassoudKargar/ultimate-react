b11fdc4e2a123f101171c0f3423c071526768a0e
MassoudKargar <75643194+MassoudKargar@users.noreply.github.com>
2025-09-25 18:47:05 +0330

    finish video 74

---
+++ travel-list/src/App.jsx
@@ -0,0 +1,87 @@
+import {useState} from "react";
+
+const initialItems = [
+  {
+    id: 1,
+    description: "Passports",
+    quantity: 2,
+    packed: false
+  },
+  {
+    id: 3,
+    description: "Charger",
+    quantity: 1,
+    packed: true
+  },
+  {
+    id: 2,
+    description: "Socks",
+    quantity: 12,
+    packed: false
+  }
+]
+
+export default function App() {
+  return (
+    <div className="app">
+      <Logo/>
+      <Form/>
+      <PackingList/>
+      <Stats/>
+    </div>)
+}
+
+function Logo() {
+  return (<h1>🌴 Far Away 🧳️</h1>)
+}
+
+function Form() {
+  const [description, setDescription] = useState("")
+  const [quantity, setQuantity] = useState(5)
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    if (description === "") return;
+    const newItem = {description, quantity, packed: false, id: Date.now()};
+    setDescription('');
+    setQuantity(1);
+  }
+
+  return (<form className="add-form" onSubmit={handleSubmit}>
+    <h3>What do you need for your 😍 trip?</h3>
+    <select
+      value={quantity}
+      onChange={(e) => setQuantity(Number(e.target.value))}>
+      {Array.from({length: 20}, (_, i) => i + 1).map(num => (
+        <option value={num} key={num}>{num}</option>))}
+    </select>
+    <input
+      type="text"
+      placeholder="Item"
+      value={description}
+      onChange={(e) => setDescription(e.target.value)}/>
+    <button type="submit">add</button>
+  </form>)
+}
+
+function Item({item, key}) {
+  return <li>
+    <span style={item.packed ? {textDecoration: "line-through"} : {}}>{item.quantity} {item.description}</span>
+    <button>❌</button>
+  </li>
+}
+
+function PackingList() {
+  return (
+    <div className="list">
+      <ul>
+        {initialItems.map((item) => (<Item item={item} key={item.id}/>))}
+      </ul>
+    </div>
+  )
+}
+
+
+function Stats() {
+  return (<fotter className="stats"><em>🧳 You have X items on your list, add you already packed x (X%)</em></fotter>)
+}
